92c3b5a96f59d12d5ea73681ba81656fe073e7ec
An Hung Nguyen <nguyenanhung1903@gmail.com>
2024-04-07 06:27:12 +0700

    change to first

--- modules/vis_attention_map.py
+++ modules/vis_attention_map.py
@@ -7,12 +7,13 @@ from .data_transforms import data_transforms
 def get_attention_map(model, img, device, ViT_config):
     head_dim = ViT_config["embed_dim"] // ViT_config["nb_head"]
     x = model.input_layer(data_transforms['train'](img).unsqueeze(0).to(device))
+    x = model.encoder[0].msa.ln1(x)
     batch_size, nb_patch, _ = x.size()
-    q = model.encoder[-1].msa.w_q(x)
+    q = model.encoder[0].msa.w_q(x)
     q = q.view(batch_size, ViT_config["nb_head"], nb_patch, head_dim)
-    k = model.encoder[-1].msa.w_k(x)
+    k = model.encoder[0].msa.w_k(x)
     k = k.view(batch_size, ViT_config["nb_head"], nb_patch, head_dim)
-    v = model.encoder[-1].msa.w_v(x)
+    v = model.encoder[0].msa.w_v(x)
     v = v.view(batch_size, ViT_config["nb_head"], nb_patch, head_dim)
 
     # inner product
