6975c1730a0071324a113a9d22c5c6c4df2c29d3
An Hung Nguyen <nguyenanhung1903@gmail.com>
2024-04-07 07:33:09 +0700

    update train.py

--- train.py
+++ train.py
@@ -12,6 +12,12 @@ from configs import ViT_config
 import torchsummary as summary
 from modules import data_transforms
 
+import datetime
+import logging
+
+timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+logging.basicConfig(filename=f"logs/{timestamp}.txt", filemode='a',level=logging.INFO)
+
 def validate_model(model, criterion):
     model.eval()
 
@@ -32,7 +38,7 @@ def validate_model(model, criterion):
     epoch_loss = running_loss / dataset_sizes['val']
     epoch_acc = running_corrects.double() / dataset_sizes['val']
 
-    print('[VALIDATE] Loss: {:.4f} Val Acc: {:.4f}'.format(epoch_loss, epoch_acc))
+    print('[VALIDATE] Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
     
     return epoch_acc, epoch_loss
     
@@ -40,6 +46,7 @@ def validate_model(model, criterion):
 def train_model(model, criterion, optimizer, num_epochs=25, save_model_path=None):
     loss_hist = {'train': [], 'val': []}
     acc_hist = {'train': [], 'val': []}
+    max_acc = 0.0
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
@@ -73,14 +80,20 @@ def train_model(model, criterion, optimizer, num_epochs=25, save_model_path=None
         acc_hist['train'].append(epoch_acc)
         print('[TRAINING] Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
         valid_epoch_acc, valid_epoch_loss = validate_model(model, criterion)
+        
+        if valid_epoch_acc > max_acc:
+            print(f"Saving best model at {save_model_path}/best.pth")
+            torch.save(model.state_dict(), f"{save_model_path}/best.pth")
+            max_acc = valid_epoch_acc
+            
+        torch.save(model.state_dict(), f"{save_model_path}/checkpoint-epoch-{epoch}.pth")
+            
         loss_hist['val'].append(valid_epoch_loss)
         acc_hist['val'].append(valid_epoch_acc)
         
-        if valid_epoch_acc > torch.tensor(acc_hist['val']).max():
-            print("Saving best model")
-            torch.save(model.state_dict(), save_model_path)
+        logging.info(f"Epoch: {epoch} - Train Loss: {epoch_loss} - Train Acc: {epoch_acc} - Val Loss: {valid_epoch_loss} - Val Acc: {valid_epoch_acc}")
         
-    return model
+    return model, (loss_hist, acc_hist)
 
 if __name__ == "__main__":
     
@@ -89,9 +102,10 @@ if __name__ == "__main__":
     args.add_argument("--batch_size", type=int, default=32, help="Batch size")
     args.add_argument("--lr", type=float, default=1e-3, help="Learning rate")
     args.add_argument("--data_dir", type=str, default="./data", help="Data directory")
-    args.add_argument("--pretrain_model_path", type=str, default="./saved_model/best.pth", help="Model path")
-    args.add_argument("--save_model_path", type=str, default="./saved_model", help="Model path")
+    args.add_argument("--pretrain_model_path", type=str, default="./saved_models/best.pth", help="Model path")
+    args.add_argument("--save_model_path", type=str, default="./saved_models", help="Model path")
     args.add_argument("--ratio", type=float, default=0.8, help="Train/Validation ratio")
+    args.add_argument("--number_workers", type=int, default=4, help="Number of workers")
     
     opt = args.parse_args()
     print(opt)
@@ -114,8 +128,8 @@ if __name__ == "__main__":
 
     # create dataloaders
     dataloaders = {
-        'train': DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True, num_workers=4),
-        'val': DataLoader(val_dataset, batch_size=opt.batch_size, shuffle=True, num_workers=4)
+        'train': DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True, num_workers=opt.number_workers),
+        'val': DataLoader(val_dataset, batch_size=opt.batch_size, shuffle=True, num_workers=opt.number_workers)
     }
 
     dataset_sizes = {
@@ -128,7 +142,7 @@ if __name__ == "__main__":
     optimizer = optim.Adam(model.parameters(), lr=opt.lr)
     
     print("Training model")
-    model = train_model(model, criterion, optimizer, num_epochs=opt.epochs, save_model_path=f"{opt.save_model_path}/best.pth")
+    model, (loss_hist, acc_hist) = train_model(model, criterion, optimizer, num_epochs=opt.epochs, save_model_path=opt.save_model_path)
     
     print("Saving model")
     torch.save(model.state_dict(), f"{opt.save_model_path}/last.pth")
