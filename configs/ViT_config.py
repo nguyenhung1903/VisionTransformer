23444eb087a78447ea90f166d57a0342047b71b7
An Hung Nguyen <nguyenanhung1903@gmail.com>
2024-04-07 07:38:23 +0700

    update vit configration file

--- configs/ViT_config.py
+++ configs/ViT_config.py
@@ -1,11 +1,11 @@
 ViT_config = {
     "in_channels": 3,
     "num_classes": 2,
-    "embed_dim": 256,
+    "embed_dim": 768,
     "patch_size": 16,
     "image_size": 224,
     "num_blocks": 3,
     "nb_head": 8,
-    "hidden_dim": 256,
+    "hidden_dim": 768,
     "dropout": 0.1,
 }
